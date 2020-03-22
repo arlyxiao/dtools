248359830eea5e32250076b74180f71a9854b4f8
arlyxiao <kingla_pei@163.com>
2020-03-22 15:06:53 +0800

    Add sqlite3

--- server/connection.js
+++ server/connection.js
@@ -2,16 +2,20 @@ const { Sequelize } = require('sequelize');
 const env = require('./config');
 
 const connection = () => {
-  return new Sequelize(env.DB_NAME, env.DB_USER, env.DB_PASSWORD, {
-    host: env.DB_HOST,
-    dialect: env.DB_DRIVE,
+  // return new Sequelize(env.DB_NAME, env.DB_USER, env.DB_PASSWORD, {
+  //   host: env.DB_HOST,
+  //   dialect: env.DB_DRIVE,
   
-    pool: {
-      max: 5,
-      min: 0,
-      idle: 10000
-    },
+  //   pool: {
+  //     max: 5,
+  //     min: 0,
+  //     idle: 10000
+  //   },
   
+  // });
+  return new Sequelize({
+    dialect: 'sqlite',
+    storage: `./store.sqlite`
   });
 };
 
